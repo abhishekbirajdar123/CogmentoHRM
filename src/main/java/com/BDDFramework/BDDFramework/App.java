67de1e13aa0eedbc92c12e71b9e3a33b83aa7f37
HP <HP@LAPTOP-KPUT2OI8>
2024-07-21 13:22:49 +0530

    BDD Framework is Created by Team Leader

---
+++ src/main/java/com/BDDFramework/BDDFramework/App.java
@@ -0,0 +1,13 @@
+package com.BDDFramework.BDDFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
